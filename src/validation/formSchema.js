6628235bee802301b57e0d64cfadcc72d20fac49
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-06 10:15:54 -0500

    created the form schema

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,25 @@
 // Here goes the schema for the form
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+    username: yup
+        .string()
+        .trim()
+        .required('Username is required')
+        .min(3, 'Username must be at least 3 characters long'),
+    email: yup
+        .string()
+        .email('Must be a valid email address')
+        .required('Email is required'),
+    role: yup
+        .string()
+        .oneOf(['instructor', 'student', 'alumni'], 'Must choose a role'),
+    civil: yup
+        .string()
+        .oneOf(['single', 'married'], 'Civil status is required'),
+    hiking: yup.boolean(),
+    reading: yup.boolean(),
+    coding: yup.boolean()
+})
+
+export default formSchema;
